608211373e7ccd385a2e8db89eefcd3cd1afb76f
Bo Lundehave Nyby <201906213@post.au.dk>
2022-05-17 18:06:08 +0200

    Implemented a rudimentary movement detector utilising the linear accelerator sensor... Works like a charm.

--- companion_app/app/src/main/java/com/example/seamlessmusiccompanionapp/conditions/MovementCondition.kt
+++ companion_app/app/src/main/java/com/example/seamlessmusiccompanionapp/conditions/MovementCondition.kt
@@ -10,32 +10,39 @@ import android.content.BroadcastReceiver
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
-import android.hardware.Sensor
-import android.hardware.SensorManager
-import android.hardware.TriggerEvent
-import android.hardware.TriggerEventListener
+import android.hardware.*
 import android.widget.Toast
 import com.example.seamlessmusiccompanionapp.BuildConfig
 import com.google.android.gms.location.*
 
 class MovementCondition : Condition {
     private val mainInstance: MainActivity = MainActivity.instance()!!
+    private var moving = false
+
+    companion object {
+        private const val ACCELERATION_THRESHOLD = 0.1 // Threshold for filtering measurement errors when still
+    }
 
     init {
         val sensorManager = mainInstance.getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        val sensor = sensorManager.getDefaultSensor(Sensor.TYPE_SIGNIFICANT_MOTION)
-        val eventListener = object : TriggerEventListener() {
-            override fun onTrigger(p0: TriggerEvent?) {
-                Log.d("proj", "Triggered!")
+        val sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
+        val eventListener = object : SensorEventListener {
+            override fun onSensorChanged(event: SensorEvent?) {
+                val values = event?.values!!
+                val x = if (values[0] >= ACCELERATION_THRESHOLD) values[0] else 0f
+                val y = if (values[1] >= ACCELERATION_THRESHOLD) values[1] else 0f
+                val z = if (values[2] >= ACCELERATION_THRESHOLD) values[2] else 0f
+
+                moving = (x+y+z > 0) // If any movement on any axis set moving to true, else false
             }
+
+            override fun onAccuracyChanged(p0: Sensor?, p1: Int) {}
         }
-        sensor.also { s -> sensorManager.requestTriggerSensor(eventListener, s) }
+        sensorManager.registerListener(eventListener, sensor, Int.MAX_VALUE)
     }
 
     override fun met(): Boolean {
-//        ensureActivityRecognitionPermissions()
-
-        return false
+        return moving
     }
 
     private fun ensureActivityRecognitionPermissions() {
